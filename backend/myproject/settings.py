64aa49a5433b0f2efee8fcd5fab26f63daa684c3
mhsharifi96 <mh.sh7676@gmail.com>
2025-09-30 23:01:18 +0330

    fix: update CORS_ALLOWED_ORIGINS

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -60,7 +60,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5174",
     "http://localhost:5173",
     "http://localhost:5174",
-    "185.204.197.117",
+    "http://185.204.197.117",
+    "https://185.204.197.117",
+
 ]
 
 CORS_ALLOW_METHODS = (
