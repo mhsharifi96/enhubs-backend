91be8b3f68efb95bb650eb33388916c1bf7949ae
mhsharifi96 <mh.sh7676@gmail.com>
2025-10-07 22:22:38 +0330

    add paginations

--- backend/lessons/views.py
+++ backend/lessons/views.py
@@ -2,9 +2,16 @@ from rest_framework import viewsets
 from .models import Audio
 from .serializers import AudioSerializer
 
+from rest_framework.pagination import PageNumberPagination
+
+class AudioPagination(PageNumberPagination):
+    page_size = 10  # number of items per page
+    page_size_query_param = 'page_size'  # allow client to set page size
+    max_page_size = 50  # maximum page size allowed
+
+
 
 class AudioLessionViewSet(viewsets.ModelViewSet):
     queryset = Audio.objects.all()
     serializer_class = AudioSerializer
-
-
+    pagination_class = AudioPagination
