65c263ae65cb7110b1ae0884fa1e8ba3e9830898
mhsharifi96 <mh.sh7676@gmail.com>
2025-10-09 01:39:31 +0330

    fix some bug

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -43,9 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'djangorestframework-simplejwt',
     # 'storages',
     'rest_framework',
+    'djangorestframework-simplejwt',
     'lessons',
 ]
 
