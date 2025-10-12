cce13f9ed1b44de63034029eba230d4553c0149c
mhsharifi96 <mh.sh7676@gmail.com>
2025-10-12 22:47:24 +0330

    add register endpoint

---
+++ backend/myproject/views.py
@@ -0,0 +1,31 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        password = request.data.get("password")
+        email = request.data.get("email")
+        username = email.split("@")
+
+        if not email or not password:
+            return Response({"error": "Email and password are required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        if User.objects.filter(username=username).exists():
+            return Response({"error": "Email already exists."}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = User.objects.create_user(username=username, password=password, email=email)
+        user.save()
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "message": "User registered successfully.",
+            "refresh": str(refresh),
+            "access": str(refresh.access_token),
+        }, status=status.HTTP_201_CREATED)
