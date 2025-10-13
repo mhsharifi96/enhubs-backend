7b4ced0ee9cfcd9191c9d05426c2aeaf6a03b15c
mhsharifi96 <mh.sh7676@gmail.com>
2025-10-13 20:38:56 +0330

    fix cores all

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
 #     "https://zp1v56uxy8rdx5ypatb0ockcb9tr6a-oci3--5173--96435430.local-credentialless.webcontainer-api.io",
 # ]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ["*"]
 
